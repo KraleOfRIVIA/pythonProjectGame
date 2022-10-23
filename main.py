f31771dc0bd776244919f946dc4434152595f159
KraleOfRIVIA <91405593+KraleOfRIVIA@users.noreply.github.com>
2022-10-23 19:59:07 +0600

    змейка

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
-# Pygame шаблон - скелет для нового проекта Pygame
-import sys
-
-import pygame as gay
+import pygame
 import random
-from pygame.color import THECOLORS
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-# Создаем игру и окно
-gay.init()
-gay.mixer.init()
-screen = gay.display.set_mode((1200, 800))
-screen.fill('white')
-font = gay.font.SysFont('couriernew', 40)
-text = font.render(str('Gay'), True, 'black')
-screen.blit(text, (50, 50))
-gay.display.set_caption("My GAY")
-clock = gay.time.Clock()
-r = gay.Rect(50, 50, 200, 200)
-gay.draw.rect(screen,'pink', r, 0)
-# Цикл игры
-running = True
-while running:
-    # Держим цикл на правильной скорости
-    clock.tick(30)
-    # Ввод процесса (события)
-    for event in gay.event.get():
-        screen.blit(text, (500, 50))
-        # check for closing window
-        if event.type == gay.QUIT:
-            gay.quit()
-            sys.exit()
-    gay.display.flip()
+from random import randrange
+RES = 800
+SIZE = 50
+
+x,y = randrange(0,RES,SIZE),randrange(0,RES,SIZE)
+apple = randrange(0,RES,SIZE),randrange(0,RES,SIZE)
+length = 1
+snake = [(x,y)]
+score = 0
+dx , dy = 0,0
+fps = 8
+pygame.init()
+sc = pygame.display.set_mode([RES,RES])
+clock = pygame.time.Clock()
+font_score = pygame.font.SysFont('Arial',26,bold=True)
+font_endGame = pygame.font.SysFont('Arial',66,bold=True)
+img = pygame.image.load('1.jpg').convert()
+while True:
+    sc.blit(img,(0,0))
+    [(pygame.draw.rect(sc,pygame.Color('orange'),(i,j,SIZE-2,SIZE-2)))for i,j in snake]
+    pygame.draw.rect(sc,pygame.Color('green'),(*apple,SIZE,SIZE))
+    render_score = font_score.render(f'Score:{score}',1,pygame.Color('pink'))
+    sc.blit(render_score,(5,5))
+    x+=dx*SIZE
+    y+=dy*SIZE
+    snake.append((x,y))
+    snake = snake[-length:]
+    if snake[-1] == apple:
+        apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+        length+=1
+        fps+=1
+        score+=1
+    if (x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE) or len(snake) != len(set(snake)):
+        while True:
+            render_endGame = font_endGame.render('Game OVER', 1, pygame.Color('red'))
+            sc.blit(render_endGame,(RES // 2-200,RES // 3))
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        dx,dy = 0,-1
+    if key[pygame.K_s]:
+        dx, dy = 0, 1
+    if key[pygame.K_a]:
+        dx,dy = -1,0
+    if key[pygame.K_d]:
+        dx,dy = 1,0
